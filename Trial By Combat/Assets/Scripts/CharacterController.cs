8a7d0d55fbde87e21e275f00bc324b5260f8ef21
Ayakben <bishopayak@gmail.com>
2019-03-01 12:47:58 -0500

    Added horizontal player movement and floor collisions

---
+++ Trial By Combat/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        //Temp use just to set the speed for now
+        speed = 3;
+        //Checks for player input
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        //Moves the player
+        transform.position = transform.position + moveInput * speed * Time.deltaTime;
+    }
+}
