aadaf13f8f0435022c5e7b7159fddb3d055cd85e
Ayakben <bishopayak@gmail.com>
2019-03-01 20:10:34 -0500

    Fist now follows player and can only move within a radius around the player

--- Trial By Combat/Assets/Scripts/Fist.cs
+++ Trial By Combat/Assets/Scripts/Fist.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Fist : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject fist;
     public float moveSpeed = 0.008f;
+    public float distance;
+    public float maxDistance = 2;
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        distance = Vector2.Distance(fist.transform.position, player.transform.position);
+        if (Input.GetMouseButton(0) && (distance <= maxDistance))
         {
             transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSpeed);
             Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             difference.Normalize();
             float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+        }
+        if (distance > maxDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);
         }
     }   
 }
